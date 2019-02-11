fa7e040219b947f12606fa92dfd10c6682d9e33c
Waleed Qadi <qadiwaleed@gmail.com>
2019-02-11 02:56:02 +0200

    re-arranged libs and added ajax support

---
+++ js/misc/ajax.js
@@ -0,0 +1,172 @@
+/**
+ * @file
+ * Extends methods from core/misc/ajax.js.
+ */
+
+(function ($, window, Drupal, drupalSettings) {
+
+  /**
+   * Attempts to find the closest glyphicon progress indicator.
+   *
+   * @param {jQuery|Element} element
+   *   A DOM element.
+   *
+   * @returns {jQuery}
+   *   A jQuery object.
+   */
+  Drupal.Ajax.prototype.findVaricon = function (element) {
+    return $(element).closest('.form-item').find('.ajax-progress.varicon')
+  };
+
+  /**
+   * Starts the spinning of the glyphicon progress indicator.
+   *
+   * @param {jQuery|Element} element
+   *   A DOM element.
+   * @param {string} [message]
+   *   An optional message to display (tooltip) for the progress.
+   *
+   * @returns {jQuery}
+   *   A jQuery object.
+   */
+  Drupal.Ajax.prototype.variconStart = function (element, message) {
+    var $varicon = this.findVaricon(element);
+    if ($varicon[0]) {
+      $varicon.addClass('varicon-spin');
+
+      // Append a message for screen readers.
+      if (message) {
+        $varicon.parent().append('<div class="sr-only message">' + message + '</div>');
+      }
+    }
+    return $varicon;
+  };
+
+  /**
+   * Stop the spinning of a glyphicon progress indicator.
+   *
+   * @param {jQuery|Element} element
+   *   A DOM element.
+   */
+  Drupal.Ajax.prototype.variconStop = function (element) {
+    var $varicon = this.findVaricon(element);
+    if ($varicon[0]) {
+      $varicon.removeClass('varicon-spin');
+    }
+  };
+
+  /**
+   * Sets the throbber progress indicator.
+   */
+  Drupal.Ajax.prototype.setProgressIndicatorThrobber = function () {
+    var $element = $(this.element);
+
+    // Find an existing glyphicon progress indicator.
+    var $varicon = this.variconStart($element, this.progress.message);
+    if ($varicon[0]) {
+      this.progress.element = $varicon.parent();
+      this.progress.varicon = true;
+      return;
+    }
+
+    // Otherwise, add a varicon throbber after the element.
+    if (!this.progress.element) {
+      this.progress.element = $(Drupal.theme('ajaxThrobber'));
+    }
+
+    if (this.progress.message) {
+      this.progress.element.after('<div class="message">' + this.progress.message + '</div>');
+    }
+
+    // If element is an input DOM element type (not :input), append after.
+    if ($element.is('input')) {
+      $element.after(this.progress.element);
+    }
+    // Otherwise append the throbber inside the element.
+    else {
+      $element.addClass("throbber-added");
+      $element.append(this.progress.element);
+    }
+  };
+
+
+  /**
+   * Handler for the form redirection completion.
+   *
+   * @param {Array.<Drupal.AjaxCommands~commandDefinition>} response
+   * @param {number} status
+   */
+  Drupal.Ajax.prototype.success = function (response, status) {
+    if (this.progress.element) {
+
+      // Stop a glyphicon throbber.
+      if (this.progress.varicon) {
+        this.variconStop(this.progress.element);
+      }
+      // Remove the progress element.
+      else {
+        this.progress.element.remove();
+      }
+
+      $(this.element).removeClass("throbber-added");
+      // Remove any message set.
+      this.progress.element.parent().find('.message').remove();
+    }
+
+    // --------------------------------------------------------
+    // Everything below is from core/misc/ajax.js.
+    // --------------------------------------------------------
+
+    if (this.progress.object) {
+      this.progress.object.stopMonitoring();
+    }
+    $(this.element).prop('disabled', false);
+
+    // Save element's ancestors tree so if the element is removed from the dom
+    // we can try to refocus one of its parents. Using addBack reverse the
+    // result array, meaning that index 0 is the highest parent in the hierarchy
+    // in this situation it is usually a <form> element.
+    var elementParents = $(this.element).parents('[data-drupal-selector]').addBack().toArray();
+
+    // Track if any command is altering the focus so we can avoid changing the
+    // focus set by the Ajax command.
+    var focusChanged = false;
+    for (var i in response) {
+      if (response.hasOwnProperty(i) && response[i].command && this.commands[response[i].command]) {
+        this.commands[response[i].command](this, response[i], status);
+        if (response[i].command === 'invoke' && response[i].method === 'focus') {
+          focusChanged = true;
+        }
+      }
+    }
+
+    // If the focus hasn't be changed by the ajax commands, try to refocus the
+    // triggering element or one of its parents if that element does not exist
+    // anymore.
+    if (!focusChanged && this.element && !$(this.element).data('disable-refocus')) {
+      var target = false;
+
+      for (var n = elementParents.length - 1; !target && n > 0; n--) {
+        target = document.querySelector('[data-drupal-selector="' + elementParents[n].getAttribute('data-drupal-selector') + '"]');
+      }
+
+      if (target) {
+        $(target).trigger('focus');
+      }
+    }
+
+    // Reattach behaviors, if they were detached in beforeSerialize(). The
+    // attachBehaviors() called on the new content from processing the response
+    // commands is not sufficient, because behaviors from the entire form need
+    // to be reattached.
+    if (this.$form) {
+      var settings = this.settings || drupalSettings;
+      Drupal.attachBehaviors(this.$form.get(0), settings);
+    }
+
+    // Remove any response-specific settings so they don't get used on the next
+    // call by mistake.
+    this.settings = null;
+  };
+
+})(jQuery, this, Drupal, drupalSettings);
---
+++ js/theme.js
@@ -0,0 +1,76 @@
+/**
+ * @file
+ * Theme hooks for the Drupal Bootstrap base theme.
+ */
+(function ($, Drupal, Bootstrap, Attributes) {
+
+  /**
+   * Fallback for theming an icon if the Icon API module is not installed.
+   */
+  if (!Drupal.icon) Drupal.icon = { bundles: {} };
+  if (!Drupal.theme.icon || Drupal.theme.prototype.icon) {
+    $.extend(Drupal.theme, /** @lends Drupal.theme */ {
+      /**
+       * Renders an icon.
+       *
+       * @param {string} bundle
+       *   The bundle which the icon belongs to.
+       * @param {string} icon
+       *   The name of the icon to render.
+       * @param {object|Attributes} [attributes]
+       *   An object of attributes to also apply to the icon.
+       *
+       * @returns {string}
+       */
+      icon: function (bundle, icon, attributes) {
+        if (!Drupal.icon.bundles[bundle]) return '';
+        attributes = Attributes.create(attributes).addClass('icon').set('aria-hidden', 'true');
+        icon = Drupal.icon.bundles[bundle](icon, attributes);
+        return '<span' + attributes + '></span>';
+      }
+    });
+  }
+
+  /**
+   * Callback for modifying an icon in the "bootstrap" icon bundle.
+   *
+   * @param {string} icon
+   *   The icon being rendered.
+   * @param {Attributes} attributes
+   *   Attributes object for the icon.
+   */
+  Drupal.icon.bundles.varicon = function (icon, attributes) {
+    attributes.addClass(['varicon', 'varicon-' + icon]);
+  };
+
+  /**
+   * Add necessary theming hooks.
+   */
+  $.extend(Drupal.theme, /** @lends Drupal.theme */ {
+
+    /**
+     * Renders a Bootstrap AJAX glyphicon throbber.
+     *
+     * @returns {string}
+     */
+    ajaxThrobber: function () {
+      return Drupal.theme('varthemeIcon', 'refresh', {'class': ['ajax-throbber', 'varicon-spin'] });
+    },
+
+    /**
+     * Renders a glyphicon.
+     *
+     * @param {string} name
+     *   The name of the glyphicon.
+     * @param {object|Attributes} [attributes]
+     *   An object of attributes to apply to the icon.
+     *
+     * @returns {string}
+     */
+    varthemeIcon: function (name, attributes) {
+      return Drupal.theme('icon', 'varicon', name, attributes);
+    }
+
+  });
+
+})(window.jQuery, window.Drupal, window.Drupal.bootstrap, window.Attributes);
